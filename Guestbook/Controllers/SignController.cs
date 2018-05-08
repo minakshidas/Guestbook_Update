95d536b58eb3401b843d8c000d71bb585b5d18a7
Minakshi Das <minakshi.das@gmail.com>
2018-05-08 11:07:36 -0700

    Initial Commit

---
+++ Guestbook/Controllers/SignController.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Guestbook.Controllers
+{
+    public class SignController : Controller
+    {
+        public ViewResult Random()
+        {
+            var Name = ' ';
+            var sign = new SignController() { Name = "Very nice" };
+            return View(sign);
+        }
+    }
+}
